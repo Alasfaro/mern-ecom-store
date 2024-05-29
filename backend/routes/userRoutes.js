2cc884a6f7922c4f37ed389856d7d57efd8a89de
Omar Al-Asfar <alasfaro@mcmaster.ca>
2024-05-29 18:32:14 -0400

    added user auth

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,38 @@
+import express from "express";
+import {
+  makeUser,
+  login,
+  // logoutCurrentUser,
+  getUsers,
+  getCurrentUserProfile,
+  updateCurrentUserProfile,
+  deleteUserById,
+  getUserById,
+  updateUserById,
+} from "../controllers/userController.js";
+
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+router
+  .route("/")
+  .post(makeUser)
+  .get(authenticate, authorizeAdmin, getUsers);
+
+router.post("/auth", login);
+// router.post("/logout", logoutCurrentUser);
+
+router
+  .route("/profile")
+  .get(authenticate, getCurrentUserProfile)
+  .put(authenticate, updateCurrentUserProfile);
+
+// ADMIN ROUTES 👇
+router
+  .route("/:id")
+  .delete(authenticate, authorizeAdmin, deleteUserById)
+  .get(authenticate, authorizeAdmin, getUserById)
+  .put(authenticate, authorizeAdmin, updateUserById);
+
+export default router;
